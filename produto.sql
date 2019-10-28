f3164fa79a683dfe6eafa323141b9b92c10b0588
Hebertphp <hebertphp@gmail.com>
2019-10-28 09:15:14 -0300

    Update produto.sql

--- produto.sql
+++ produto.sql
@@ -6,3 +6,10 @@ create table produto(
  estoque int not null,
  dt_criacao date
 );
+insert into produto (nome, preco, categoria,estoque) values ('HD Ssd 480gb',309.50,'ssd',10);
+insert into produto (nome, preco, categoria,estoque) values ('HD Ssd 240gb',188.00,'ssd',15);
+insert into produto (nome, preco, categoria,estoque, dt_criacao) values ('HD Ssd 100gb',135.00,'ssd',20,'2019-10-26');
+insert into produto (nome, preco, categoria,estoque, dt_criacao) values ('Pen Drive 32GB',24.90,'pendrive',50,'2019-10-27');
+insert into produto (nome, preco, categoria,estoque, dt_criacao) values ('Pen Drive 128GB',109.53,'pendrive',50,'2019-10-27');
+insert into produto (nome, preco, categoria,estoque, dt_criacao) values ('Mouse Gamer 12.000 DPI ',159.99,'mouse',5,'2019-10-28');
+insert into produto (nome, preco, categoria,estoque, dt_criacao) values ('Mouse Gamer Pro M7 Rgb ',51,24 ,'mouse',5,'2019-10-28');
