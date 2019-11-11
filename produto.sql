5e6e28311f071a512003f59ade6548ca4c76adf8
Hebertphp <hebertphp@gmail.com>
2019-11-11 11:09:10 -0300

    Update produto.sql

--- produto.sql
+++ produto.sql
@@ -1,6 +1,6 @@
 create table produto(
  id_prod int not null primary key auto_increment,
- nome varchar (50) not null,
+ nome varchar (100) not null,
  preco decimal (10,2) not null,
  categoria varchar(40) not null,
  estoque int not null,
